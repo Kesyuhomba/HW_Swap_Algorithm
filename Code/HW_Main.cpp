2618cd5b5687dfb1793fe0ce3234135e3b7f17b7
Kesyuhomba <123911054+Kesyuhomba@users.noreply.github.com>
2023-03-12 23:51:02 -0500

    Update HW_Main.cpp

--- Code/HW_Main.cpp
+++ Code/HW_Main.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
-
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 // Variable - START
-//	define variables that are global between the two Variable markers
+int arrSize;
+int i; // to indicate an integer
+float element; // numerical emplimitation of elements
+vector<float> list; 
 
-// Variable - END
+int main()
+{
+  cout << "Enter the number size of arrays you desire: "; //insert elemets arrays
+cin >> arraySize; // character input
+while (!cin)
+  {
+cout << "The input you inserted was not comprehendable \n" //display the message
+  << "Please enter another value: "; //message when there is a error detected
+cin.clear();
+cin.ignore(100, '\n');
+cin >> arraySize;
+    }
+  cout << endl;
 
-// Function/Method Declaration - START
-//	define function/method declaration signatures between the two markers
+cout << "The array: "; //inserted elements not sorted
+for (int i = 0; i < list.size(); i++)
+  {
+cout << list.at(i) << "";
+    }
+cout << endl;
+sort(list.begin(), list.end()); //display the arrays sorted
+cout << "sorted array: ";
+for (int i = 0; i < list.size(); i++)
+  {
+cout << list.at(i) << "";
+    }
+cout << endl;
+return 0;
+  }
 
-// Function/Method Declaration - END
-
-int main() {
-	cout << "Hello World!";
-}
-
-// Function/Method Definition - START
-//	define function/method declaration signatures between the two markers
-
-// Function/Method Definition - END
